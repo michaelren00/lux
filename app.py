830be2467c28f3173327ff6d3bf49a81438584d4
michaelren00 <47461509+michaelren00@users.noreply.github.com>
2020-02-02 10:38:17 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from twilio.rest import Client
 from flask import Flask, request
 import json
+import random
 
 # from flask import Flask
 app = Flask(__name__)
@@ -8,7 +9,26 @@ app = Flask(__name__)
 with open("LOGIN.txt") as file:
 	login = file.readlines()
 
-
+def getMessage():
+	list_phrases = ["Have a good day!",
+"Keep your head high!",
+"Dreams turn into reality with the proper steps",
+"You can do it!",
+"You will always find a way!",
+"You're doing great! Keep it up",
+"Hope begins in the dark. Don't give up!",
+"Get that bread!",
+"Stay focused!",
+"Believe in yourself, and you will succeed!",
+"Life can be hard sometimes, but don't give up!",
+"Even if you feel down, know that tomorrow will be a new day!",
+"Always know that you are loved and cared by someone!",
+"The struggle you’re in today is developing the strength you need tomorrow.",
+"It’s not whether you get knocked down, it’s whether you get up.",
+"Keep your face to the sunshine and you cannot see a shadow.",
+"You’re braver than you believe, and stronger than you seem, and smarter than you think."]
+	return list_phrases[random.randint(0,len(list_phrases))]
+	
 @app.route("/sendMessage", methods=["POST"])
 def sendMessage():
 
@@ -19,9 +39,12 @@ def sendMessage():
 	auth_token = login[3]
 	client = Client(account_sid, auth_token)
 
+	#returnSet = getMessage(previous)
+	#previous = returnSet[0]
+
 	message = client.messages \
 	    .create(
-	         body='josh big smart',
+	         body=getMessage(), 
 	         from_=login[5],
 
 	         #status_callback='http://postb.in/1234abcd',
